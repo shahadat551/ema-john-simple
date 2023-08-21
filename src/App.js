f7bb102c6352714d5dc96bffdf310a16ed6c970b
shahadat551 <shahadathossen551@gmail.com>
2023-08-21 22:51:26 +0600

    ema-john-simple

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from "react";
+
+function App() {
+  const nayoks=["hero Alom","tuku","vadimma"]
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>fuck</h1>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
